0b990513807c88581fb5597bbe41d4643bd43ddb
Humoyunbek Tursunniyazov <humoyunbektursunniyazov@gmail.com>
2024-02-08 18:40:44 +0500

    main

--- client/components/sidebar/sidebar.tsx
+++ client/components/sidebar/sidebar.tsx
@@ -6,6 +6,8 @@ import { Bell, Home, User } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import SidebarItem from "@/components/sidebar/sidebar-item";
+import SidebarPostButton from "./sidebar-post-button";
+import SidebarAccount from "./sidebar-account";
 
 const Sidebar = ({ user }: { user: any }) => {
   const { data: session, status }: any = useSession();
@@ -25,22 +27,22 @@ const Sidebar = ({ user }: { user: any }) => {
   ];
 
   return (
-    <div
-      className={`className="sticky left-0 top-0 h-screen lg:w-[266px] w-fit flex flex-col justify-between py-4 pl-2"`}
-    >
-      {/*  Mobile Sidebar  */}
+    <div className="sticky left-0 top-0 h-screen lg:w-[266px] w-fit flex flex-col justify-between py-4 pl-2">
       <div className={`flex flex-col space-y-2`}>
         <div
           className={`rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-sky-300 hover:bg-opacity-10 cursor-pointer transition`}
         >
           <Image src={`/images/logo.svg`} alt={`logo`} width={56} height={56} />
         </div>
-        {sidebarItems.map((item, index) => (
-          <Link key={index} href={item.path}>
+        {sidebarItems.map((item) => (
+          <Link key={item.path} href={item.path}>
             <SidebarItem {...item} />
           </Link>
         ))}
+
+        <SidebarPostButton />
       </div>
+      <SidebarAccount user={user} />
     </div>
   );
 };
